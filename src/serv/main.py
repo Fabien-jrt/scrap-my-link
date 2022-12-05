03fdd1cb7e3ca2b215bd7ebaec1c08951ee3291b
Fabien <67684689+Fabien-jrt@users.noreply.github.com>
2022-12-05 21:37:32 +0100

    Adds flask server (#1)
    * fix code formating
    
    * Replace math by if clause
    
    * Add a default url to Dockerfile
    
    * Use dependencies intervals
    
    * Add src/ folder
    
    * Add Flask
    
    * Add flask form
    
    * Improve form.html
    
    * Add link scrap to text
    
    * Update README.md

---
+++ src/serv/main.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, request, render_template
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_text_links(url: str):
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, "html.parser")
+    res = []
+    for link in soup.find_all("a"):
+        res.append(link.get('href'))
+    return res
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_form():
+    return render_template('form.html')
+
+@app.route("/", methods=['POST'])
+def get_form():
+    url = request.form['link']
+    res = get_text_links(url)
+    return res
+
+if __name__=="__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4444)))
