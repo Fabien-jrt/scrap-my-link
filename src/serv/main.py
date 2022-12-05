a1918a471ce15fa3251d258b401bbfa22732f22a
Fabien-Jrt <67684689+Fabien-jrt@users.noreply.github.com>
2022-12-05 20:02:52 +0100

    Add flask form

--- src/serv/main.py
+++ src/serv/main.py
@@ -1,11 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def render_form():
+    return render_template('form.html')
+
+@app.route("/", methods=['POST'])
+def get_form():
+    url = request.form['text']
+    return url
 
 if __name__=="__main__":
     app.run(host=os.getenv('IP', '0.0.0.0'), 
